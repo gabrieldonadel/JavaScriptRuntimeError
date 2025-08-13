dcfd0edd79ae79c8973f6e2b9912e6e46522b342
Gabriel Donadel <donadeldev@gmail.com>
2025-08-13 15:54:35 -0300

    throw JS error from c++

--- ReproducerApp/shared/NativeSampleModule.cpp
+++ ReproducerApp/shared/NativeSampleModule.cpp
@@ -6,6 +6,18 @@ NativeSampleModule::NativeSampleModule(std::shared_ptr<CallInvoker> jsInvoker)
     : NativeSampleModuleCxxSpec(std::move(jsInvoker)) {}
 
 std::string NativeSampleModule::reverseString(jsi::Runtime& rt, std::string input) {
+  jsi::String jsMessage = jsi::String::createFromUtf8(rt, "My custom js error message");
+  jsi::Value error = rt
+    .global()
+    .getProperty(rt, "Error")
+    .asObject(rt)
+    .asFunction(rt)
+    .callAsConstructor(rt, {
+      jsi::Value(rt, jsMessage)
+    });
+
+  throw jsi::JSError(rt, jsi::Value(rt, error));
+
   return std::string(input.rbegin(), input.rend());
 }
 
